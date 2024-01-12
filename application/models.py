9924bd543c51888f44587446a7286665a988e221
Abhiek187 <achaudhuri2011@yahoo.com>
2024-01-11 20:18:47 -0500

    Update dependencies

--- application/models.py
+++ application/models.py
@@ -1,27 +1,30 @@
 import flask
-from application import db
+from mongoengine import Document, IntField, StringField
 from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.Document):
-	user_id = db.IntField(unique=True)
-	first_name = db.StringField(max_length=50)
-	last_name = db.StringField(max_length=50)
-	email = db.StringField(max_length=30, unique=True)
-	password = db.StringField()
 
-	def set_password(self, password):
-		self.password = generate_password_hash(password)
+class User(Document):
+    user_id = IntField(unique=True)
+    first_name = StringField(max_length=50)
+    last_name = StringField(max_length=50)
+    email = StringField(max_length=30, unique=True)
+    password = StringField()
 
-	def get_password(self, password):
-		return check_password_hash(self.password, password)
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
 
-class Course(db.Document):
-	courseID = db.StringField(max_length=10, unique=True)
-	title = db.StringField(max_length=100)
-	description = db.StringField(max_length=255)
-	credits = db.IntField()
-	term = db.StringField(max_length=25)
+    def get_password(self, password):
+        return check_password_hash(self.password, password)
 
-class Enrollment(db.Document):
-	user_id = db.IntField()
-	courseID = db.StringField(max_length=10)
+
+class Course(Document):
+    courseID = StringField(max_length=10, unique=True)
+    title = StringField(max_length=100)
+    description = StringField(max_length=255)
+    credits = IntField()
+    term = StringField(max_length=25)
+
+
+class Enrollment(Document):
+    user_id = IntField()
+    courseID = StringField(max_length=10)
